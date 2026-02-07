665a594033eab3b984501692355881a5deeaeef6
Astra-max <waoremaxwel17@gmail.com>
2026-02-08 00:57:26 +0300

    added more api auth endpoints

--- go/0x00-go/pkg/routes/api.go
+++ go/0x00-go/pkg/routes/api.go
@@ -7,7 +7,9 @@ import (
 
 func ApiRoutes(r *server.Router) {
 	r.GET("/", handlers.AuthUser)
-	r.POST("/", handlers.AuthUser)
+	r.POST("/login", handlers.AuthUser)
 	r.POST("/sign-up", handlers.SingUpUser)
+	r.POST("/forgot-pass", handlers.ForgotPass)
+	r.POST("/refresh", handlers.RefreshToken)
 	r.GET("/health", handlers.Health)
 }
