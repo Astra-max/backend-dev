d3d553f80d7472392944ea5cd02eedff15cbd467
Astra-max <waoremaxwel17@gmail.com>
2026-04-21 17:50:45 +0100

    sql: selecting column by descending order

--- databases/0x00-sql-db/select.sql
+++ databases/0x00-sql-db/select.sql
@@ -9,3 +9,7 @@ SELECT user_id, first_name, age, email FROM users;
 -- select columns with distinct value/not other occurences
 
 SELECT DISTINCT first_name FROM users;
+
+-- selecting table columns ordering by age in descending order
+
+SELECT * FROM user ORDER BY age DESC;
