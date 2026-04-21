ca7dbd944d69acfb03763ae64bd2361a96c298d5
Astra-max <waoremaxwel17@gmail.com>
2026-04-21 17:45:34 +0100

    sql: select all rows with distinct values/first name

--- databases/0x00-sql-db/select.sql
+++ databases/0x00-sql-db/select.sql
@@ -5,3 +5,7 @@ SELECT * FROM user;
 --- select specific columns, id,name,age and email
 
 SELECT user_id, first_name, age, email FROM users;
+
+-- select columns with distinct value/not other occurences
+
+SELECT DISTINCT first_name FROM users;
