13aee83690c619c5fa7a79f9b4a93a97568b8752
Astra-max <waoremaxwel17@gmail.com>
2026-04-22 17:49:36 +0100

    sql: returning the least value in a column

--- databases/0x00-sql-db/functions.sql
+++ databases/0x00-sql-db/functions.sql
@@ -9,3 +9,7 @@ SELECT MAX(age) FROM users;
 --- getting average of all values in  a column
 
 SELECT AVG(age) FROM users;
+
+--- selecting the minimum value within a column
+
+SELECT MIN(age) FROM users;
