e150834388a147cb33fcd2941e381d7d139d9da9
Astra-max <waoremaxwel17@gmail.com>
2026-04-22 18:39:11 +0100

    sql: handling zero division err with nullif

--- databases/0x00-sql-db/arritmetics.sql
+++ databases/0x00-sql-db/arritmetics.sql
@@ -5,3 +5,7 @@ SELECT age, age * 20 AS new_age FROM users;
 -- basic multiplication arrithmetics
 
 SELECT 7 * 7;
+
+--- handling zero division error
+
+SELECT COALESCE(10 / NULLIF(0, 0), "zero division erro");
