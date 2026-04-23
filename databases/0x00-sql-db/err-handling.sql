9bfccee9503cee0edc0de2fea2209bbef01c2cb1
Astra-max <waoremaxwel17@gmail.com>
2026-04-23 18:14:29 +0100

    sql: do update to column on error instead of retuning nothing

--- databases/0x00-sql-db/err-handling.sql
+++ databases/0x00-sql-db/err-handling.sql
@@ -3,5 +3,11 @@
 INSERT INTO users(id, name) VALUES(1, 'astra');
 
 ---- handling error
+--- columns must be unique to use it
 
 INSERT INTO users(id, name) VALUES(1, 'astra') ON CONFLICT (id) DO NOTHING;
+
+---- update the current results to incomming req instead of nothing
+--- EXCLUDED replaces the current field throwing constraint violation err
+
+INSERT INTO users(id, name) VALUES(1, 'astra') ON CONFLICT (id) DO UPDATE SET id = EXCLUDED.id;
