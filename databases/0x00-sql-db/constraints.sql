32a76ee099c0501eaee2a1730fbaf854b21d74ec
Astra-max <waoremaxwel17@gmail.com>
2026-04-23 17:29:55 +0100

    sql: adding unique constraint to table fields

--- databases/0x00-sql-db/constraints.sql
+++ databases/0x00-sql-db/constraints.sql
@@ -11,5 +11,14 @@ CREATE TABLE users(
 ALTER TABLE users DROP CONSTRAINT person_pkey;
 
 --- adding primary key for fields
+-- takes an array of columns to be made unique PRIMARY KEY(user_id, email)
 
 ALTER TABLE users ADD PRIMARY KEY(user_id);
+
+--- unique constrait allow us to have unique values for a given column
+
+--- adding it by create table method
+
+CREATE TABLE users(
+    email TEXT NOT NULL UNIQUE,
+);
