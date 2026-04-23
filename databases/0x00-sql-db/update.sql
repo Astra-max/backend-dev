ce7633f0cb9c5db9528c876cff0755376116385b
Astra-max <waoremaxwel17@gmail.com>
2026-04-23 18:00:31 +0100

    sql: updating columns that have meet a certain condition

--- databases/0x00-sql-db/update.sql
+++ databases/0x00-sql-db/update.sql
@@ -1,3 +1,7 @@
 --- updating all columns/dangerous in production
 
 UPDATE users SET id = 90;
+
+---- updating columns that has made certain condutions
+
+UPDATE users SET id = 90 WHERE age > 34 AND age < 90;
