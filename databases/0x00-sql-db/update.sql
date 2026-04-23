dfa336f4dfe3641bb4d0112478aba3912961a37b
Astra-max <waoremaxwel17@gmail.com>
2026-04-23 18:03:09 +0100

    sql: updating several fields in a table

--- databases/0x00-sql-db/update.sql
+++ databases/0x00-sql-db/update.sql
@@ -5,3 +5,7 @@ UPDATE users SET id = 90;
 ---- updating columns that has made certain condutions
 
 UPDATE users SET id = 90 WHERE age > 34 AND age < 90;
+
+---- updating several columns
+
+UPDATE users SET id = 90, first_name = 'jame' WHERE id = 40;
