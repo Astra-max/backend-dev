3c8314a3edb39931697eeff1321ca51996c18d8b
Astra-max <waoremaxwel17@gmail.com>
2026-04-24 18:36:43 +0100

    Sql: inner joins

--- databases/0x00-sql-db/joins.sql
+++ databases/0x00-sql-db/joins.sql
@@ -1,2 +1,18 @@
+CREATE TABLE cars(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    car_id SERIAL REFERENCES cars(id)
+);
+
+
+--- innner joins -- joins two tables and create new column for refence column
+
+
 SELECT * FROM users
 JOIN cars ON user_id = car_id;
