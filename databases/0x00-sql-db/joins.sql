8c5c3bc3c3845f69890a01daf2d428d348e73d85
Astra-max <waoremaxwel17@gmail.com>
2026-04-24 18:42:27 +0100

    sql: left joins returns all record including col with not forign key relationship

--- databases/0x00-sql-db/joins.sql
+++ databases/0x00-sql-db/joins.sql
@@ -12,7 +12,14 @@ CREATE TABLE users(
 
 
 --- innner joins -- joins two tables and create new column for refence column
+--- returns rows that has a match
 
 
 SELECT * FROM users
 JOIN cars ON user_id = car_id;
+
+
+---- left joins -- joins two tables list all records even if there is no match
+
+SELECT * FROM users
+LEFT JOIN cars ON users.user_id = cars.id;
